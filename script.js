e84b89b1e7de0cbc83512c7625144840a33047f9
David Bijl <143999252+bijldavid@users.noreply.github.com>
2025-03-03 10:19:11 +0100

    update

--- script.js
+++ script.js
@@ -451,8 +451,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // HIER AANPASSEN
   // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
   const connectionMapThema1 = [
-    [2, 4],
-    [3, 3],
+    [0, 1],
+    [1, 2],
     [5, 0],
     [5, 1],
     [5, 2]
